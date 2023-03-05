2adeca0d73d1bdcb8489ad62d1357f9fa64ff476
Dener Souza Assis Faria <58788344+denersouza2@users.noreply.github.com>
2023-03-05 09:27:27 -0300

    Add files via upload

---
+++ src/main/java/com/lojaonline/LojaEcommerce/LojaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.lojaonline.LojaEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojaonline/LojaEcommerce/LojaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaonline.LojaEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
